d28fced0234624357255cfe7425a5d3d63697bec
chengtien <113504735+chengtien@users.noreply.github.com>
2022-11-24 15:04:06 +0800

    Add files via upload

--- HW1.R
+++ HW1.R
@@ -9,9 +9,9 @@ ggplot(data=country_unemployment_rate,
                      colour=Country,group=Country))+
   geom_point()+
   geom_line()+
-  scale_colour_manual(values = c("#FFCCFF","#FFCC33","#FF6633",
-                                 "#009900","#66CCFF","#0033FF",
-                                 "#000066","#9999FF","#CC66CC"))+
+  scale_colour_manual(values = c("#CC3300","#FF6600","#CCCCCC",
+                                 "#999999","#666666","#669999",
+                                "#000000","#99CCCC","#CC6600"))+
   labs(title = "世界各國近五年失業率",
        subtitle = "橫軸為年份,縱軸為失業率(單位為%)",
        caption="資料來源:https://www.stat.gov.tw/ct.asp?xItem=37200&ctNode=517&mp=4",
